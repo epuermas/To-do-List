f0c627919609f6f352bff4effa39832b2649b099
Eduardo Puermas <eduardopuermas@hotmail.com>
2019-11-23 23:19:35 +0000

    Initial Update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,86 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export class Home extends React.Component {
+	constructor() {
+		super();
 
-//create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+		this.state = {
+			todos: [
+				{ done: false, title: "Wake up", id: Math.random() * 10 },
+				{
+					done: false,
+					title: "Have an existential breakdown",
+					id: Math.random() * 10
+				},
+				{ done: false, title: "Eat breakfast", id: Math.random() * 10 },
+				{ done: false, title: "Go to work", id: Math.random() * 10 }
+			],
+			taskInput: ""
+		};
+	}
+
+	handleFormSubmit(e) {
+		e.preventDefault();
+		this.setState({
+			todos: this.state.todos.concat([
+				{
+					title: this.state.taskInput,
+					done: false,
+					id: Math.random() * 10
+				}
+			])
+		});
+		return false;
+	}
+
+	destroyTask(taskId) {
+		this.setState({
+			todos: this.state.todos.filter(task => task.id != taskId)
+		});
+	}
+
+	render() {
+		var tasksToRender = this.state.todos.map(task => {
+			return (
+				<li key={task.id}>
+					<div className="view">
+						<label>{task.title}</label>
+						<button
+							className="destroy"
+							onClick={() => this.destroyTask(task.id)}
+						/>
+					</div>
+				</li>
+			);
+		});
+		return (
+			<section className="todoapp">
+				<header className="header">
+					<h1>todo list</h1>
+					<form onSubmit={this.handleFormSubmit.bind(this)}>
+						<input
+							autoFocus={true}
+							className="new-todo"
+							placeholder="What's the next task?"
+							value={this.state.taskInput}
+							onChange={evt =>
+								this.setState({ taskInput: evt.target.value })
+							}
+						/>
+					</form>
+				</header>
+				<section className="main">
+					<ul className="todo-list">{tasksToRender}</ul>
+				</section>
+				<footer className="footer">
+					<span align="center" className="todo-count">
+						<strong>
+							{this.state.todos.filter(key => !key.done).length}
+						</strong>{" "}
+						items left
+					</span>
+				</footer>
+			</section>
+		);
+	}
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,11 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
 
-//include bootstrap npm library into the bundle
-import "bootstrap";
-
-//include your index.scss file into the bundle
+// Add SCSS files to bundle
 import "../styles/index.scss";
 
-//import your own components
+//Importing the Home class component
 import { Home } from "./component/home.js";
 
-//render your react application
+// Render application to DOM
 ReactDOM.render(<Home />, document.querySelector("#app"));
